77b1c147e3447676b537eac9322f3ca93fe78dc9
yugodev <ygnk0805@outlook.jp>
2025-11-01 17:36:25 +0900

    デュアルブート環境のUbuntu test

--- build/ros2_firebase_bridge/CMakeFiles/ament_cmake_python_build_ros2_firebase_bridge_egg.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for custom commands dependencies management for ament_cmake_python_build_ros2_firebase_bridge_egg.
--- build/ros2_firebase_bridge/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/workspaces/mobility-ros2-firebase/install/ros2_firebase_bridge'
